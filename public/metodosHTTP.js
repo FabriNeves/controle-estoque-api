e2cff329e6f892ceaff54c382aca3fcf797ff0ea
FaSnow <eng.fbneves@gmail.com>
2023-03-23 22:46:17 -0300

    Readme.md

--- public/metodosHTTP.js
+++ public/metodosHTTP.js
@@ -1,54 +1,105 @@
+// Revisar todos os itens !!! 
 
-class metodosHTTP {
-
-    async getData() {
-        try {
-            const response = await fetch(baseURL);
-            return response.json();
-        } catch (e) {
-            console.error(e);
-        };
-    };
-
-
+class ProdutosAPI {
+    baseURL = "http://localhost:3000/cadastroProdutos";
+  
+    async getItem() {
+      try {
+        const response = await fetch(this.baseURL);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    }
+  
     async createItem(item) {
-        try {
-            const response = await fetch(baseURL, {
-                method: 'POST',
-                body: JSON.stringify(item),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.json();
-        } catch (e) {
-            console.error(e);
-        }
+      try {
+        const response = await fetch(this.baseURL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(item),
+        });
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     }
-
+  
+    async updateItem(id, item) {
+      try {
+        const response = await fetch(`${this.baseURL}/${id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(item),
+        });
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    }
+  
     async deleteItem(id) {
-        try {
-            const response = await fetch(`${baseURL}/${id}`, {
-                method: 'DELETE'
-            });
-            return response.json();
-        } catch (e) {
-            console.error(e);
-        }
+      try {
+        const response = await fetch(`${this.baseURL}/${id}`, {
+          method: "DELETE",
+        });
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     }
-
-    async updateItem(id, data) {
-        try {
-            const response = await fetch(`${baseURL}/${id}`, {
-                method: 'PUT',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            return response.json();
-        } catch (e) {
-            console.error(e);
-        }
+  
+    async searchItems({ nome, marca, qtd }) {
+      const params = new URLSearchParams({
+        nome: nome || "",
+        marca: marca || "",
+        qtd: qtd || "",
+      });
+  
+      const url = `${this.baseURL}/busca?${params.toString()}`;
+  
+      try {
+        const response = await fetch(url);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
     }
-}
+  
+    async getItensPagination({ itensPorPagina = 10, pagina = 1 }) {
+      const params = new URLSearchParams({
+        itensPorPagina: itensPorPagina,
+        pagina: pagina,
+      });
+  
+      const url = `${this.baseURL}/pagina?${params.toString()}`;
+  
+      try {
+        const response = await fetch(url);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    }
+  
+    async getItensById(id) {
+      try {
+        const response = await fetch(`${this.baseURL}/${id}`);
+        return response.json();
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    }
+  }
+  
