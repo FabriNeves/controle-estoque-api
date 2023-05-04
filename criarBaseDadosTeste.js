62c49ddfe2c5bb5fc757c6559b475f8c90901eab
FaSnow <eng.fbneves@gmail.com>
2023-05-04 02:14:45 -0300

    Criação de Metodos Registro de Movimentação

--- criarBaseDadosTeste.js
+++ criarBaseDadosTeste.js
@@ -1,6 +1,6 @@
 import sequelize from "./src/config/config.js";
 import Produto from "./src/models/produtosModel.js";
-import { Entrada, Saida } from "./src/models/controleFluxoModel.js"
+import Movimentacao from "./src/models/controleFluxoModel.js";
 
 import { listaJSONplus } from "./produtos.js";
 
@@ -18,10 +18,12 @@ await sequelize.sync({ force: true });
 listaJSONplus.forEach(async input => {
 
     const produto = await Produto.create(input);
-    await Entrada.create({
+    await Movimentacao.create({
+        nome:produto.nome,
+        tipo:"entrada",        
         produto_id:produto.id,
-        qtd_in: produto.qtd,
-        data_in: new Date()
+        qtd: produto.qtd,
+        data: new Date()
       })
 });
 
--- src/controllers/fluxoController.js
+++ src/controllers/fluxoController.js
@@ -1,41 +1,94 @@
-import { validate } from "../services/validate.js";
+import Sequelize from 'sequelize';
+import Movimentacao from '../models/controleFluxoModel.js';
+import Produto from '../models/produtosModel.js';
+class controleFluxo {
+    static async entrada(produto,qtd, data_in) {
 
-class fluxoEstoque {
-  // REGISTRO
-  static async registroIO(req, res) {
-    const { id } = req.params;
-    const { qtd } = req.body;
+        data_in = data_in ? data_in : new Date();
 
-    if (qtd === 0) {
-      return res.status(400).json({ error: "A quantidade não pode ser igual a zero." });
-    } else if (qtd < 0) {
-      if (Math.abs(qtd) > produto.qtd) {
-        return res.status(400).json({ error: "A quantidade a ser retirada é maior do que a quantidade atual." });
-      }
-    }
-
-    try {
-      const produto = await Produto.findOne({ where: { id } });
-
-      if (produto) {
-        const validacao = validate(produto, qtd);
-
-        
-        const diferenca = Math.abs(qtd);
-        if (qtd > 0) {
-          await Entrada.create({ qtd_in: diferenca, data_in: new Date() });
-        } else {
-          await Saida.create({ qtd_out: diferenca, data_out: new Date() });
+        try {
+            await Movimentacao.create({
+                nome: produto.nome,
+                tipo: "entrada",
+                produto_id: produto.id,
+                qtd: qtd,
+                data: data_in
+            })
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
         }
 
-        await produto.update({ qtd: produto.qtd + qtd });
-
-        res.status(200).json({ produto, alerta: validacao });
-      } else {
-        res.status(404).send("Produto não encontrado.");
-      }
-    } catch (error) {
-      res.status(500).send(error);
     }
-  }
+    static async saida(produto,qtd, data_out) {
+
+        data_out = data_out ? data_out : new Date();
+
+        try {
+            await Movimentacao.create({
+                nome: produto.nome,
+                tipo: "saida",
+                produto_id: produto.id,
+                qtd: -(qtd),
+                data: data_out
+            })
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+
+    }
+
+    static async registrarEntrada(req, res) {
+        const { id } = req.params;
+        const { qtd_in, data } = req.body;
+        try {
+          const produto = await Produto.findOne({ where: { id } });
+          if (!produto) {
+            return res.status(404).send('Produto não encontrado.');
+          }
+          if (qtd_in <= 0) {
+            return res.status(400).send('A quantidade de entrada deve ser maior que zero.');
+          }
+          const qtd_total = produto.qtd + qtd_in;
+          await produto.update({ qtd: qtd_total });
+          await controleFluxo.entrada(produto, qtd_in, data);
+          res.status(200).send('Entrada registrada com sucesso.');
+        } catch (error) {
+          console.error(error);
+          res.status(500).send('Ocorreu um erro ao registrar a entrada.');
+        }
+      }
+      
+      static async registrarSaida(req, res) {
+        const { id } = req.params;
+        const { qtd_out, data } = req.body;
+        
+        try {
+          const produto = await Produto.findOne({ where: { id } });
+          if (!produto) {
+            return res.status(404).send("Produto não encontrado.");
+          }
+          const { qtd } = produto;
+          if (qtd < qtd_out) {
+            return res.status(409).send("Quantidade indisponível para saída.");
+          }
+          const qtd_total = qtd - qtd_out;
+          const result = await produto.update({ qtd: qtd_total });
+          if (result) {
+            const movimentacao = await controleFluxo.saida(produto, qtd_out, data);
+            if (!movimentacao) {
+              return res.status(500).send("Erro ao registrar saída na movimentação.");
+            }
+            return res.status(200).json(result);
+          }
+        } catch (error) {
+          res.status(500).send(error);
+        }
+      }
+      
 }
+
+export default controleFluxo;
