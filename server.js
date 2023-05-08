94fe3acc86282dcdc41f7337b3fd296c85c241be
FaSnow <eng.fbneves@gmail.com>
2023-05-08 01:28:37 -0300

    Teste Usando uma porta Publica da Internet.

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ app.listen(porta,() => {
     // http://localhost:3000/
     console.log(`Servidor escutando em http://localhost:${porta}/`);
 })
+// app.listen(porta, '0.0.0.0', () => {
+//   console.log(`Servidor escutando em http://0.0.0.0:${porta}/`);
+// });
