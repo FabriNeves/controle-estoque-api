ad0cf12cd2ae60d980ce23b85bd2040d5755112a
FaSnow <eng.fbneves@gmail.com>
2023-05-16 23:52:37 -0300

    Validate no padrão Airbnb

--- src/services/validate.js
+++ src/services/validate.js
@@ -1,19 +1,18 @@
-export function validate(obj, qtd) {
-    
-    if (qtd < obj.min_qtd) {
-      return {
-        bool:true,
-        message: "A quantidade informada está abaixo do limite mínimo permitido.",
-        min_qtd: obj.min_qtd,
-      };
-    } else if (qtd > obj.max_qtd) {
-      return {
-        bool:true,
-        message: "A quantidade informada está acima do limite máximo permitido.",
-        max_qtd: obj.max_qtd,
-      };
-    } else {
-      return { bool:false};
-    }
+function validate(obj, qtd) {
+  if (qtd < obj.min_qtd) {
+    return {
+      bool: true,
+      message: 'A quantidade informada está abaixo do limite mínimo permitido.',
+      min_qtd: obj.min_qtd,
+    };
+  } if (qtd > obj.max_qtd) {
+    return {
+      bool: true,
+      message: 'A quantidade informada está acima do limite máximo permitido.',
+      max_qtd: obj.max_qtd,
+    };
   }
-  
+  return { bool: false };
+}
+
+export default validate;
